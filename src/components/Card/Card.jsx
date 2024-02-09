3894769a857e0639afc14e936cccd35bf1298aee
Guillaume Cornu <guillaumecornu@guillaume.cornu>
2024-02-09 12:07:27 +0100

    feat(cards): add card components in home page

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import styles from "./Card.module.scss";
+
+/**
+ * @param {Object} props
+ * @param {string} props.imageUrl - url of the background image displayed in the card
+ * @param {string} props.title - title displayed in the card
+ * @returns {React.JSX.Element} card component
+ */
+export default function Card({ imageUrl, title = "" }) {
+  const style = {
+    backgroundImage: `url(${imageUrl})`,
+  };
+
+  return (
+    <div className={styles.container} style={style}>
+      <p>{title}</p>
+    </div>
+  );
+}
--- src/pages/Home-page/Home-page.jsx
+++ src/pages/Home-page/Home-page.jsx
@@ -1,16 +1,25 @@
 import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
+import data from "../../data/data.json";
 import HomePageBannerBackgroundImage from "../../assets/images/home-page-banner-background.jpg";
-import "./Home-page.module.scss";
+import styles from "./Home-page.module.scss";
 
 /**
  * @returns {React.JSX.Element} home page component
  */
 export default function HomePage() {
   return (
-    <Banner
-      backgroundImageOpacity={0.6}
-      imageUrl={HomePageBannerBackgroundImage}
-      text="Chez vous, partout et ailleurs"
-    />
+    <div className={styles.container}>
+      <Banner
+        backgroundImageOpacity={0.6}
+        imageUrl={HomePageBannerBackgroundImage}
+        text="Chez vous, partout et ailleurs"
+      />
+      <div className={styles.cardContainer}>
+        {data.map((data) => (
+          <Card imageUrl={data.cover} key={data.id} title={data.title} />
+        ))}
+      </div>
+    </div>
   );
 }
