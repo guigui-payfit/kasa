a544e10d243a436124b52d2148ca838e810a9993
Guillaume Cornu <guillaumecornu@guillaume.cornu>
2024-02-15 17:15:21 +0100

    feat(about-page): make collapse components expandable

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+
 import chevronUpIcon from "../../assets/images/chevron-up.svg";
 import styles from "./Collapse.module.scss";
 
 /**
  * @param {Object} props
+ * @param {string} props.content - text content displayed when the collapse component is expanded
  * @param {string} props.title - title displayed in the (expanded or not) collapse component
  * @returns {React.JSX.Element} collapse component
  */
-export default function Collapse({ title }) {
+export default function Collapse({ content, title }) {
+  const [hasBeenTouched, setHasBeenTouched] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <div className={styles.titleContainer}>
-      <p>{title}</p>
-      <svg aria-label="See the description." role="img">
-        <title>See the description.</title>
-        <use xlinkHref={`${chevronUpIcon}#chevron-up`}></use>
-      </svg>
-    </div>
+    <section
+      style={isExpanded ? { backgroundColor: "rgba(246, 246, 246, 0.45)" } : {}}
+    >
+      <div className={styles.titleContainer}>
+        <p>{title}</p>
+        <svg
+          aria-label="See the description."
+          onClick={() => {
+            setHasBeenTouched(true);
+            setIsExpanded(!isExpanded);
+          }}
+          role="img"
+          style={isExpanded ? { transform: "rotate(-180deg)" } : {}}
+        >
+          <title>See the description.</title>
+          <use xlinkHref={`${chevronUpIcon}#chevron-up`}></use>
+        </svg>
+      </div>
+      <div
+        className={`${styles.contentContainer} ${
+          isExpanded ? styles.expanded : styles.collapsed
+        } ${hasBeenTouched ? styles.touched : ""}`}
+      >
+        <p>{content}</p>
+      </div>
+    </section>
   );
 }
--- src/pages/About-page/About-page.jsx
+++ src/pages/About-page/About-page.jsx
@@ -15,16 +15,28 @@ export default function AboutPage() {
       />
       <ul>
         <li>
-          <Collapse title="Fiabilité" />
+          <Collapse
+            content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+            title="Fiabilité"
+          />
         </li>
         <li>
-          <Collapse title="Respect" />
+          <Collapse
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            title="Respect"
+          />
         </li>
         <li>
-          <Collapse title="Service" />
+          <Collapse
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            title="Service"
+          />
         </li>
         <li>
-          <Collapse title="Sécurité" />
+          <Collapse
+            content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            title="Sécurité"
+          />
         </li>
       </ul>
     </div>
